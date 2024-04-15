bc2dc177824a1c54ddeb2db3e80be03ca7f00cd5
Archangel_SDY <Archangel.SDY@gmail.com>
2024-04-15 21:06:14 +0800

    Include user_defined_tags in JSON response

--- PatreonDownloader.Implementation/PatreonPageCrawler.cs
+++ PatreonDownloader.Implementation/PatreonPageCrawler.cs
@@ -31,8 +31,9 @@ namespace PatreonDownloader.Implementation
 
         //TODO: Research possibility of not hardcoding this string
         private const string CrawlStartUrl = "https://www.patreon.com/api/posts?" +
-                                             "include=user%2Cattachments%2Ccampaign%2Cpoll.choices%2Cpoll.current_user_responses.user%2Cpoll.current_user_responses.choice%2Cpoll.current_user_responses.poll%2Caccess_rules.tier.null%2Cimages.null%2Caudio.null" +
+                                             "include=user%2Cuser_defined_tags%2Cattachments%2Ccampaign%2Cpoll.choices%2Cpoll.current_user_responses.user%2Cpoll.current_user_responses.choice%2Cpoll.current_user_responses.poll%2Caccess_rules.tier.null%2Cimages.null%2Caudio.null" +
                                              "&fields[post]=change_visibility_at%2Ccomment_count%2Ccontent%2Ccurrent_user_can_delete%2Ccurrent_user_can_view%2Ccurrent_user_has_liked%2Cembed%2Cimage%2Cis_paid%2Clike_count%2Cmin_cents_pledged_to_view%2Cpost_file%2Cpost_metadata%2Cpublished_at%2Cpatron_count%2Cpatreon_url%2Cpost_type%2Cpledge_url%2Cthumbnail_url%2Cteaser_text%2Ctitle%2Cupgrade_url%2Curl%2Cwas_posted_by_campaign_owner" +
+                                             "&fields[post_tag]=tag_type%2Cvalue" +
                                              "&fields[user]=image_url%2Cfull_name%2Curl" +
                                              "&fields[campaign]=show_audio_post_download_links%2Cavatar_photo_url%2Cearnings_visibility%2Cis_nsfw%2Cis_monthly%2Cname%2Curl" +
                                              "&fields[access_rule]=access_rule_type%2Camount_cents" +
@@ -358,7 +359,8 @@ namespace PatreonDownloader.Implementation
                         jsonEntry.Type != "access-rule" && 
                         jsonEntry.Type != "reward" && 
                         jsonEntry.Type != "poll_choice" &&
-                        jsonEntry.Type != "poll_response")
+                        jsonEntry.Type != "poll_response" &&
+                        jsonEntry.Type != "post_tag")
                     {
                         string msg = $"Verification for {jsonEntry.Id}: Unknown type for \"included\": {jsonEntry.Type}";
                         _logger.Error(msg);
